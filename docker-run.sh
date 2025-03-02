5b7b32cab77baf3197ebb450181d480a26822910
Fredrik Haglund <92490571+fr3h4g@users.noreply.github.com>
2025-03-02 23:30:34 +0100

    network host

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker rm tofso-vv-control
-docker run --name tofso-vv-control --env-file .env --device /dev/gpiochip0 tofso-vv-control
+docker run --name tofso-vv-control --env-file .env --network=host --device /dev/gpiochip0 tofso-vv-control
